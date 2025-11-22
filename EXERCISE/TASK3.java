58fb331ad8cfa83b289157e04b33a876e8b341b7
CIICCB8 <kiretin@gmail.com>
2025-11-22 08:49:30 +0000

    task3

---
+++ EXERCISE/TASK3.java
@@ -0,0 +1,18 @@
+package EXERCISE;
+public class TASK3 {
+
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = a;
+        String c = "Hello";
+        String d = b + "!";
+        
+        boolean b1 = a ==b;
+        boolean b2 = d.equals(b+"!");
+        boolean b3 = !c.equals(a);
+    
+        if (b1 && b2 && b3) {
+            System.out.println("success");
+        }
+    }
+}
