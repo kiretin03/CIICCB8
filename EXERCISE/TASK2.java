c702486c8b47146b31572df0f3fd68ce22d3b17e
CIICCB8 <kiretin@gmail.com>
2025-11-22 08:38:38 +0000

    TASK2

---
+++ EXERCISE/TASK2.java
@@ -0,0 +1,28 @@
+package EXERCISE;
+
+public class TASK2 {
+
+    public static void main(String[] args) {
+
+        // primitives (except long and double)
+        byte b = 3;
+        short s = 11;
+        int i = 0;
+        float f = 2.0f;
+        char H = 'H';
+        char l = 'l';
+        char w = 'w';
+        boolean t = true;
+
+        // build required output
+        String output =
+                "" + H + b + s + i + " " +
+                w + i + "r" + s + "d " +
+                f + " " +
+                t;
+
+        System.out.println(output);
+    }
+
+
+}
